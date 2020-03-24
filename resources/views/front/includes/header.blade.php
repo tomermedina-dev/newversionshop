74912340812210887de7fc6e1d5581d9c9b855e7
Tomer Medina <tomercatahanmedina@gmail.com>
2020-03-24 13:58:33 +0800

    Added front includes

---
+++ resources/views/front/includes/header.blade.php
@@ -0,0 +1,70 @@
+<nav class="d-flex  navbar navbar-icon-top navbar-expand-md navbar-light  " style="background-color: #2f3640;">
+<div class="container">
+    <a  class="navbar-brand">
+      <img  width="80px" src="{{ asset('images/logo_transpa.png') }}">
+    </a>
+    <button type="button" class="navbar-toggler bg-light" data-toggle="collapse" data-target="#nav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- <input type="text" class="form-control input-search-header" id="first_name" placeholder="Search "> -->
+    <div  style="width:100%;">
+      <div class="input-group md-form form-sm form-2 pl-0">
+        <input class="form-control  input-search-header" type="text" placeholder="Search" aria-label="Search">
+        <div class="input-group-append ">
+          <span class="input-group-text input-search-header" >
+            <i class="fas fa-search text-white"aria-hidden="true"></i></span>
+        </div>
+      </div>
+
+      <div class="header-contact-details">
+         <i class="fa fa-envelope text-white"aria-hidden="true"></i>
+         <span  > newversion-support@email.com</span>
+         <i class="fa fa-phone-alt "aria-hidden="true"></i>
+         <span  > +63 123 458</span>
+         <i class="fab fa-facebook-square"></i>
+      </div>
+    </div>
+
+    <div id="nav" class="navbar-collapse collapse w-100 order-3 dual-collapse2" >
+         <ul class="navbar-nav ml-auto white">
+             <li class="nav-item ">
+               <a class="nav-link" href="#">
+                 <!-- <i class="fa fa-home"></i> -->
+                 <h5>Home</h5>
+                 </a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link active " href="#">
+                 <!-- <i class="fas fa-user-plus"></i> -->
+                 <h5>register</h5>
+                 </a>
+             </li>
+             <li class="nav-item ">
+               <a class="nav-link" href="#">
+                 <!-- <i class="fa fa-cube"></i> -->
+                 <h5>Products</h5>
+                 </a>
+             </li>
+             <li class="nav-item ">
+               <a class="nav-link" href="#">
+                 <!-- <i class="fas fa-car"></i> -->
+                 <h5>autos</h5>
+                 </a>
+             </li>
+             <li class="nav-item ">
+               <a class="nav-link" href="#">
+                 <!-- <i class="fas fa-user-plus"></i> -->
+                 <h5>about</h5>
+                 </a>
+             </li>
+             <li class="nav-item ">
+               <a class="nav-link" href="#">
+                 <i class="fa fa-shopping-cart"></i>
+                 <!-- <h5>cart</h5> -->
+                 </a>
+             </li>
+         </ul>
+     </div>
+
+</div>
+</nav>
