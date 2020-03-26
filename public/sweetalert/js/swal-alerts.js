927111d4ef486640e938d79c494f86dbb30bac8d
Tomer Medina <tomercatahanmedina@gmail.com>
2020-03-26 20:23:14 +0800

    Added SWAL

---
+++ public/sweetalert/js/swal-alerts.js
@@ -0,0 +1,8 @@
+function errorSwal() {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops... Something went wrong.',
+    text:  'Please contact the web admin.',
+            // footer: '<a href>Why do I have this issue?</a>'
+    })
+}
