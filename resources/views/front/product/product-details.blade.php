65abf1241cd38f795eccea9b9d804290547b80b3
Erron <rocksvain@gmail.com>
2020-03-28 07:42:35 +0800

    Product Details (100% done)

--- resources/views/front/product/product-details.blade.php
+++ resources/views/front/product/product-details.blade.php
@@ -66,7 +66,7 @@
   </div>
   <div class="col-lg-7">
     <div class="nv-labels">
-      <div class="nv-details-header d-flex justify-content-between">
+      <div class="nv-details-header d-flex justify-content-between align-items-start">
         <div class="nv-name nv-font-bc">
           PRODUCT NAME
         </div>
@@ -154,12 +154,160 @@
         Car Model 1
       </div>
     </div>
+    <div class="col-lg-3">
+      <div class="nv-label">
+        Warranty Period
+      </div>
+      <div class="nv-value">
+        5 months
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="nv-label">
+        Warranty Type
+      </div>
+      <div class="nv-value">
+        Local Supplier Warranty
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="nv-label">
+        SKU
+      </div>
+      <div class="nv-value">
+        1234567_PH_0987654
+      </div>
+    </div>
   </div>
 </div>
 
 <div class="nv-line"></div>
 
+<div class="nv-related-products">
+  <div class="nv-header nv-font-bc">
+    You might also like
+  </div>
+
+  <div class="row">
+    <div class="col-lg-3">
+      <div class="card nv-product-card nv-default-box-shadow">
+        <div class="nv-img-container">
+          <div class="nv-product-actions d-flex justify-content-center align-items-center">
+            <a href="#"><i class="fas fa-cart-plus"></i></a>
+            <div class="nv-heart-checkbox">
+              <input type="checkbox" id="favorite2">
+              <label for="favorite2"><i class="far fa-heart"></i></label>
+            </div>
+
+          </div>
+          <img src="" >
+        </div>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <div class="">
+            <div class="nv-name nv-font-bc">
+              Product 1
+            </div>
+            <div class="nv-category">
+              Category 1
+            </div>
+          </div>
+          <div class="nv-price nv-font-bc">
+            P 250.00
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="card nv-product-card nv-default-box-shadow">
+        <div class="nv-img-container">
+          <div class="nv-product-actions d-flex justify-content-center align-items-center">
+            <a href="#"><i class="fas fa-cart-plus"></i></a>
+            <div class="nv-heart-checkbox">
+              <input type="checkbox" id="favorite3">
+              <label for="favorite3"><i class="far fa-heart"></i></label>
+            </div>
+
+          </div>
+          <img src="" >
+        </div>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <div class="">
+            <div class="nv-name nv-font-bc">
+              Product 1
+            </div>
+            <div class="nv-category">
+              Category 1
+            </div>
+          </div>
+          <div class="nv-price nv-font-bc">
+            P 250.00
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="card nv-product-card nv-default-box-shadow">
+        <div class="nv-img-container">
+          <div class="nv-product-actions d-flex justify-content-center align-items-center">
+            <a href="#"><i class="fas fa-cart-plus"></i></a>
+            <div class="nv-heart-checkbox">
+              <input type="checkbox" id="favorite4">
+              <label for="favorite4"><i class="far fa-heart"></i></label>
+            </div>
+
+          </div>
+          <img src="" >
+        </div>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <div class="">
+            <div class="nv-name nv-font-bc">
+              Product 1
+            </div>
+            <div class="nv-category">
+              Category 1
+            </div>
+          </div>
+          <div class="nv-price nv-font-bc">
+            P 250.00
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3">
+      <div class="card nv-product-card nv-default-box-shadow">
+        <div class="nv-img-container">
+          <div class="nv-product-actions d-flex justify-content-center align-items-center">
+            <a href="#"><i class="fas fa-cart-plus"></i></a>
+            <div class="nv-heart-checkbox">
+              <input type="checkbox" id="favorite5">
+              <label for="favorite5"><i class="far fa-heart"></i></label>
+            </div>
+
+          </div>
+          <img src="" >
+        </div>
+        <div class="card-body d-flex justify-content-between align-items-center">
+          <div class="">
+            <div class="nv-name nv-font-bc">
+              Product 1
+            </div>
+            <div class="nv-category">
+              Category 1
+            </div>
+          </div>
+          <div class="nv-price nv-font-bc">
+            P 250.00
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
+
+</div>
+
+
 
 <script type="text/javascript">
     $(document).ready(function () {
