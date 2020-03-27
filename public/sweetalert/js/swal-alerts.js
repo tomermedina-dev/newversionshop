0bc70fd27c96bf2d6f6f4bed998412a036d24acf
Tomer Medina <tomercatahanmedina@gmail.com>
2020-03-27 18:27:20 +0800

    SWal alerts added

--- public/sweetalert/js/swal-alerts.js
+++ public/sweetalert/js/swal-alerts.js
@@ -1,8 +1,44 @@
-function errorSwal() {
+function swalWentWrong() {
   Swal.fire({
     icon: 'error',
     title: 'Oops... Something went wrong.',
     text:  'Please contact the web admin.',
             // footer: '<a href>Why do I have this issue?</a>'
+    })
+}
+
+
+function swalLoading(message) {
+    let timerInterval
+    swal.fire({
+        title: '',
+        html: '<div class="nv-swal-message-holder"><h5>'+message+'</h5></div>',
+
+        onBeforeOpen: function()  {
+
+            Swal.showLoading()
+        },
+        allowOutsideClick : false
+    }).then(function(response) {})
+}
+function swalError(message) {
+    Swal.fire({
+        icon : 'error' ,
+        title: '',
+        html: '<div class="nv-swal-message-holder"><h5>'+message+'</h5></div>',
+    })
+}
+function swalSuccess(message) {
+    Swal.fire({
+        icon : 'success' ,
+        title: '',
+        html: '<div class="nv-swal-message-holder"><h5>'+message+'</h5></div>',
+    })
+}
+function swalWarning(message) {
+    Swal.fire({
+        icon : 'warning' ,
+        title: '',
+        html: '<div class="nv-swal-message-holder"><h5>'+message+'</h5></div>',
     })
 }
