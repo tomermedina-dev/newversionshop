51a8141064261b307a1fe9856e321ec36871e44a
Tomer Medina <tomercatahanmedina@gmail.com>
2020-03-27 18:20:07 +0800

    Added front pages controller

---
+++ app/Http/Controllers/Front/BladePagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BladePagesController extends Controller
+{
+    //
+    public function getRegistrationIndex()
+    {
+      // code...
+      return view('front.user.register');
+    }
+
+    public function getLoginIndex()
+    {
+      // code...
+      return view('front.user.login');
+    }
+
+    public function getValidateIndex()
+    {
+      // code...
+        return view('front.user.validation');
+    }
+}
