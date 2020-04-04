a503a4427520c49e5472d40533a767f5bb90e09a
Tomer Medina <tomercatahanmedina@gmail.com>
2020-04-04 12:15:38 +0800

    Added new routes

--- routes/front-web.php
+++ routes/front-web.php
@@ -1,41 +1,41 @@
 <?php
 Route::group(['prefix'=> 'users' ,],
   function() {
-// Registration
-Route::post('/register', 'Admin\UserController@creteNewOrEditUser')->name('admin.users.register');
-Route::post('/validate-email', 'Admin\UserController@validateEmail')->name('admin.users.validate.email');
-Route::post('/', 'Admin\UserController@creteNewOrEditUser')->name('admin.users.register');
-Route::post('/forgot-password', 'Admin\UserActivationController@forgotPassword')->name('admin.users.forgot.password');
-Route::post('/login', 'Front\LoginController@validateAccount')->name('front.users.register');
 
-Route::group(['prefix'=>'activation',],
-  function() {
-    Route::get('/generate', 'Admin\UserActivationController@generateSMSCode')->name('admin.users.generate');
-    Route::get('/resend/{userId}', 'Admin\UserActivationController@generateUserActivation')->name('admin.users.activation.generate');
-    Route::get('/validate/{userId}/{activationCode}', 'Admin\UserActivationController@validateActivationCode')->name('admin.users.activation.generate');
-  });
+    Route::post('/register', 'Admin\UserController@creteNewOrEditUser')->name('admin.users.register');
+    Route::post('/validate-email', 'Admin\UserController@validateEmail')->name('admin.users.validate.email');
+    Route::post('/', 'Admin\UserController@creteNewOrEditUser')->name('admin.users.register');
+    Route::post('/forgot-password', 'Admin\UserController@forgotPassword')->name('admin.users.forgot.password');
+    Route::post('/login', 'Front\LoginController@validateAccount')->name('front.users.register');
 
-Route::group(['prefix'=>'edit',],
-  function() {
-    Route::post('/details', 'Admin\UserController@creteNewOrEditUser')->name('admin.users.edit.details');
-    Route::get('/status/{id}/{status}', 'Admin\UserController@updateUserStatus')->name('admin.users.edit.status');
-  });
+    Route::group(['prefix'=>'activation',],
+      function() {
+        Route::get('/generate', 'Admin\UserActivationController@generateSMSCode')->name('admin.users.generate');
+        Route::get('/resend/{userId}', 'Admin\UserActivationController@generateUserActivation')->name('admin.users.activation.generate');
+        Route::get('/validate/{userId}/{activationCode}', 'Admin\UserActivationController@validateActivationCode')->name('admin.users.activation.generate');
+      });
+
+    Route::group(['prefix'=>'edit',],
+      function() {
+        Route::post('/details', 'Admin\UserController@creteNewOrEditUser')->name('admin.users.edit.details');
+        Route::get('/status/{id}/{status}', 'Admin\UserController@updateUserStatus')->name('admin.users.edit.status');
+      });
 
 
-// Address
-Route::group(['prefix'=>'address',],
-  function() {
-        Route::get('/new/{userId}/{addressDetails}/{notes}', 'Admin\UserAddressController@createOrEditUserAddress')->name('admin.users.address.new');
-        Route::get('/edit/{userId}/{addressDetails}/{notes}/{addressId}', 'Admin\UserAddressController@createOrEditUserAddress')->name('admin.users.address.edit');
-        Route::get('/delete/{addressId}', 'Admin\UserAddressController@deleteUserAddress')->name('admin.users.address.delete');
-        Route::get('/default/{userId}/{addressId}', 'Admin\UserAddressController@setDefaultAddress')->name('admin.users.address.set.default');
-  });
+    // Address
+    Route::group(['prefix'=>'address',],
+      function() {
+            Route::get('/new/{userId}/{addressDetails}/{notes}', 'Admin\UserAddressController@createOrEditUserAddress')->name('admin.users.address.new');
+            Route::get('/edit/{userId}/{addressDetails}/{notes}/{addressId}', 'Admin\UserAddressController@createOrEditUserAddress')->name('admin.users.address.edit');
+            Route::get('/delete/{addressId}', 'Admin\UserAddressController@deleteUserAddress')->name('admin.users.address.delete');
+            Route::get('/default/{userId}/{addressId}', 'Admin\UserAddressController@setDefaultAddress')->name('admin.users.address.set.default');
+      });
 
-// Unit
-Route::group(['prefix'=>'unit',],
-  function() {
-        Route::get('/new/{userId}/{carBrand}/{model}/{vin}/{plateNumber}', 'Admin\UserUnitsController@createNewUserUnit')->name('admin.users.unit.new');
-  });
+    // Unit
+    Route::group(['prefix'=>'unit',],
+      function() {
+            Route::get('/new/{userId}/{carBrand}/{model}/{vin}/{plateNumber}', 'Admin\UserUnitsController@createNewUserUnit')->name('admin.users.unit.new');
+      });
 
 });
 
@@ -57,14 +57,25 @@ Route::group(['prefix'=>'wishlist',],
 
 Route::group(['prefix'=>'products',],
   function() {
-    Route::get('/', 'Front\BladePagesController@getProductBladeIndex')->name('front.products.index');
+    Route::get('/', 'Front\BladePagesController@redirectToProducts')->name('front.products.index');
+    Route::get('/{category}', 'Front\BladePagesController@getProductBladeIndex')->name('front.products.index');
     Route::get('/details', 'Front\BladePagesController@getProductDetailsIndex')->name('front.products.details');
-    Route::get('/cart', 'Front\BladePagesController@getProductCartIndex')->name('front.products.details');
+    Route::get('/list/{type}' ,'Admin\ProductController@getProductByType')->name('admin.products.all');
+    Route::get('/list/search/{value}' ,'Admin\ProductController@getProductBySearch')->name('admin.products.all');
   });
 
+Route::group(['prefix'=>'mail',],
+    function() {
+      Route::get('/forgot-password/{userId}', 'Admin\EmailController@sendForgotPassword')->name('front.mail.forgot');
+      Route::group(['prefix'=>'layout',],
+          function() {
+            Route::get('/forgot-password', 'Front\BladePagesController@getMailLayoutForgot')->name('front.mail.layout.forgot');
 
+          });
+    });
+Route::get('/cart', 'Front\BladePagesController@getProductCartIndex')->name('front.products.details');
 Route::get('/', 'Front\BladePagesController@getHomeIndex')->name('front.home.index');
 Route::get('/register', 'Front\BladePagesController@getRegistrationIndex')->name('front.register.index');
 Route::get('/login', 'Front\BladePagesController@getLoginIndex')->name('front.login.index');
-Route::get('/validate', 'Front\BladePagesController@getValidateIndex')->name('front.login.index');
+Route::get('/forgot', 'Front\BladePagesController@getForgotIndex')->name('front.forgot.index');
 Route::get('/logout', 'Admin\UserController@logout')->name('front.logout  ');
