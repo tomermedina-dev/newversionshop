a3735a10b54699770ca080539ed8b0c8000280be
Tomer Medina <tomercatahanmedina@gmail.com>
2020-04-04 12:16:34 +0800

    New functions for login

---
+++ public/admin/js/login.js
@@ -0,0 +1,52 @@
+Vue.component('nv-component-login' , {
+  data : function(){
+    return {
+      username : "" ,
+      password : ""
+    }
+  } ,
+  template : `<div class="container nv-login-form " style="width:70%;">
+      <label for="username"  >Username</label>
+      <div class="form-group">
+        <div class="input-group-prepend">
+           <span class="input-group-text nv-input-icon-plain"   ><i class="left fa fa-user-circle text-black"aria-hidden="true"></i></span>
+           <input v-model="username" type="text" class="left form-control nv-input-custom" id="username" placeholder="Enter your username">
+         </div>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <div class="input-group-prepend">
+           <span class="input-group-text nv-input-icon-plain"   ><i class="left fa fa-lock text-black"aria-hidden="true"></i></span>
+          <input v-model="password" type="password" class="form-control nv-input-custom" id="password" placeholder="Enter your password">
+         </div>
+      </div>
+      <div class="container d-flex justify-content-center">
+        <button type="button" v-on:click="submitLogin" class="btn nv-btn-txt-white" name="button">Log Me in</button>
+      </div>
+    </div>` ,
+    methods : {
+      submitLogin : function(){
+        const t = this ;
+        if(!t.username.trim() || !t.password.trim()){
+          swalError("Please enter your username and password")
+        }else{
+          var formLogin = new FormData();
+          formLogin.append('username' , t.username);
+          formLogin.append('password' , t.password);
+          axios.post('/admin/login' ,formLogin ).then(function(response) {
+            var response = response.data;
+
+            if(response == 1){
+              window.location.href = "/admin/home";
+            }else {
+              swalError(response);
+            }
+          }).catch(function (error) {
+            swalWentWrong();
+          }).finally(function(response){});
+        }
+      }
+    }
+
+} );
+new Vue({el:"#nv-login"});
