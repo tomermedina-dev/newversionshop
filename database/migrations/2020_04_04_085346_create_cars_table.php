b22247311e1b8184c36818ee1e4e4d253e72223a
Tomer Medina <tomercatahanmedina@gmail.com>
2020-04-11 02:20:35 +0800

    New table

---
+++ database/migrations/2020_04_04_085346_create_cars_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('car_manufacturer');
+            $table->string('car_model');
+            $table->string('color');
+            $table->string('price');
+            $table->longText('description');
+            $table->string('status');
+            $table->timestamps();
+        });
+        DB::statement('ALTER TABLE cars CHANGE id id INT(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
