8cad022a07476f26a9b244e0e646bd16a9bcbcfe
Tomer Medina <tomercatahanmedina@gmail.com>
2020-04-11 02:17:19 +0800

    Minor changes

--- database/views/products.sql
+++ database/views/products.sql
@@ -1,4 +1,4 @@
 create view product_vw as
-  SELECT b.type_name as 'product_categ', A.*
+  SELECT b.type_name as 'product_categ',fGetFirstImage(A.id , 'product') as 'product_image' , A.*
 FROM products A LEFT JOIN product_types B
 ON A.type_id = B.id;
