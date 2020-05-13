abfc737edafd66b0ecd369c7d555698b46f20f69
Tomer Medina <tomercatahanmedina@gmail.com>
2020-05-13 23:58:50 +0800

    [Fix] Login button size issue on mobile

--- public/front/js/login.js
+++ public/front/js/login.js
@@ -7,7 +7,7 @@ Vue.component('nv-component-login' , {
       password : ""
     }
   } ,
-  template : `<div class="container nv-login-form " style="width:70%;">
+  template : `<div class="container nv-login-form " style="width:85%;">
       <label for="username"  >Username</label>
       <div class="form-group">
         <div class="input-group-prepend">
@@ -22,17 +22,14 @@ Vue.component('nv-component-login' , {
           <input v-model="password" type="password" class="form-control nv-input-custom" id="password" placeholder="Enter your password">
          </div>
       </div>
-      <div class="container d-flex justify-content-center">
-        <div class="d-flex flex-nowrap">
-          <div class="order-1 p-2">  <button type="button" v-on:click="submitLogin" class="btn nv-btn-txt-white" name="button">Log Me in</button> </div>
-          <div class="order-2 p-2 nv-horizontal-center">
+      <div >
+         <button type="button" v-on:click="submitLogin" class="btn btn-md nv-btn-txt-white w-100 float-left" name="button">Log Me in</button>
+          <div class="nv-horizontal-center float-left" style="display:none;">
             <div class="custom-control custom-checkbox  ">
               <input v-model="password" type="checkbox" class="custom-control-input" id="customCheck1">
               <!---<label class="custom-control-label" for="customCheck1">Remember Me</label>-->
               <a   href="/forgot"   class="badge text-black"  style="font-size: 1em;cursor:pointer;margin:1%;">Forgot Password?</a>
             </div>
-          </div>
-
         </div>
 
       </div>
