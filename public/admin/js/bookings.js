02b03712df136d3d25ef05ee9d9d2d149a2ab2e0
Tomer Medina <tomercatahanmedina@gmail.com>
2020-05-18 17:29:50 +0800

    [Feature] Added approval on schedule change request

--- public/admin/js/bookings.js
+++ public/admin/js/bookings.js
@@ -1,7 +1,9 @@
 var bookedServices = new Vue({
   el : "#nv-bss-content" ,
   data : {
-    bookedServicesList : []
+    bookedServicesList : [] ,
+    index : 0 ,
+    booking : ''
   } ,
   methods : {
     loadBookedServices : function(){
@@ -9,13 +11,33 @@ var bookedServices = new Vue({
       axios.
       get('/admin/services/booking/all/new').then(function(response) {
         t.bookedServicesList = response.data;
+        console.log(t.bookedServicesList);
       }).catch(function(error) {
         swalWentWrong();
       });
     } ,
     pad : function(value) {
       return pad(value , 10);
+    } ,
+    requestResponse : function(serviceId , response) {
+      const t = this;
+      var status;
+      if(response == 'reject'){
+        status = 'X';
+      }else{
+        status = '1';
+      }
+      swalLoading("Submitting response please wait..")
+      axios.get('/admin/services/booking/request-change-date/'+serviceId+'/'+status).
+      then(function(response) {
+        t.loadBookedServices();
+        swalSuccess("Approval response has been saved.");
+      }).catch(function (error) {
+        swalWentWrong(error);
+      })
     }
+  } ,
+  mounted (){
+    this.loadBookedServices();
   }
 });
-bookedServices.loadBookedServices();
