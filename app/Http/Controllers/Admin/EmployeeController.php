5620ab2049c71cb133d00e7d523564ca7d9ba31f
Tomer Medina <tomercatahanmedina@gmail.com>
2020-05-22 19:06:20 +0800

    [Feature] Added employee registration

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Admin\UserEmployee;
+use Illuminate\Support\Facades\Hash;
+use Session;
 class EmployeeController extends Controller
 {
     //
@@ -13,5 +15,29 @@ class EmployeeController extends Controller
       // code...
       $employees = UserEmployee::where('status' , $status)->get();
       return json_encode($employees);
+    }
+    public function getRegisterIndex()
+    {
+      // code...
+      return view('admin.pages.register');
+    }
+    public function registerEmployee(Request $request)
+    {
+      // code...
+      $credentials = [
+        'first_name' => $request->first_name ,
+        'last_name' => $request->last_name ,
+        'username' =>  $request->username,
+        'password' =>  Hash::make($request->password),
+        'contact_num' => $request-> contact,
+        'address' => $request->address ,
+        'email' => $request->email ,
+        'status' => '1'
+      ];
+      $employee = UserEmployee::create($credentials);
+      $userId = str_pad( $employee->id, 10, '0', STR_PAD_LEFT) ;
+      Session::put('userId' , $userId);
+      Session::put('role' , 'employee');
+      return json_encode($employee);
     }
 }
---
+++ public/admin/js/register.js
@@ -0,0 +1,89 @@
+var formRegister  = new FormData();
+new Vue({
+  el : "#employee-register-content" ,
+  data  : {
+    first_name : "" ,
+    username : "" ,
+    last_name : "" ,
+    email : "" ,
+    password : "" ,
+    confirm_password: "" ,
+    contact : "" ,
+    address : "" ,
+  } ,
+  methods : {
+    register : function(){
+      if (this.validateFields()){
+        swalWarning(this.validateFields());
+      }else{
+        this.getFields();
+        this.submitRegistration();
+      }
+
+    } ,
+    submitRegistration : function(){
+      swalLoading('Sending registration...');
+      axios.post('/admin/employee/register',formRegister)
+      .then(function(response){
+        window.location.href = "/admin/page/home";
+      })
+      .catch(function (error) {
+        swalWentWrong();
+      }).finally(function (response) {});
+      formRegister = new FormData();
+    } ,
+    getFields : function(){
+      const t = this ;
+      formRegister.append('username' , t.username);
+      formRegister.append('password' , t.password);
+      formRegister.append('first_name' , t.first_name);
+      formRegister.append('last_name' , t.last_name);
+      formRegister.append('email' , t.email);
+      formRegister.append('contact' , t.contact);
+      formRegister.append('address' , t.address);
+    },
+    validateFields : function() {
+      if (!this.first_name){
+        return "Please enter your first name.";
+      }
+      if (!this.last_name){
+        return "Please enter your last name.";
+      }
+      if (!this.username){
+        return "Please enter your username.";
+      }
+      if (!this.password){
+        return "Please enter your password.";
+      }
+      if(this.password != this.confirm_password || !this.confirm_password){
+       return "Those password didn't match.";
+      }
+      if (!this.contact){
+        return "Please enter your contact number.";
+      }
+
+
+      if (!this.address){
+        return "Please enter your address.";
+      }
+
+
+
+      if (registerPassErr == 1){
+         return "Please enter atleast 6 character password.";
+      }
+    } ,
+    validateEmailFormat : function(email){
+      return validateEmailFormat(email);
+    } ,
+    validatePassword : function(){
+        if (this.password.length > 5){
+          $("#err_pass").text("");
+          registerPassErr = 0;
+        }else{
+          $("#err_pass").text("Please enter atleast 6 character password.");
+          registerPassErr = 1;
+        }
+    },
+  }
+})
