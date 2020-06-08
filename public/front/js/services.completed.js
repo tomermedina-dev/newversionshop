b5c0c37ecd640fa45e0e9dc67ac15a87e5a5f0c1
Tomer Medina <tomercatahanmedina@gmail.com>
2020-06-08 09:10:33 +0800

    [Feature] Added Completed ,  Pending and Rejected booking (Front)

---
+++ public/front/js/services.completed.js
@@ -0,0 +1,28 @@
+var jobMonitoring = new Vue({
+  el : "#nv-job-monitoring-list" ,
+  data :{
+    jobList : []
+  } ,
+  methods : {
+    loadJobList : function () {
+      const t = this;
+      axios.
+      get('/user/services/jobs/list/completed/'+userId).
+      then(function (response) {
+        t.jobList = response.data;
+      }).catch(function(error) {
+        swalWentWrong(error);
+      })
+    } ,
+    viewQR : function(assignmentId , jobId , employeeId) {
+      var value = window.location.hostname + "-qr-job-"+pad(assignmentId ,10)+"-"+jobId+"-"+employeeId;
+      window.open('/admin/qr/generate/'+value);
+    } ,
+    pad : function (value) {
+      return pad(value , 10);
+    }
+  } ,
+  mounted () {
+    this.loadJobList();
+  }
+})
